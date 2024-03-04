89e7e007cabdacbecd24a83517c344e3caf25700
Norbaeocystin <baranrastislav@gmail.com>
2024-03-03 23:56:28 -0300

    experimental api

---
+++ tests/rest_api_test.rs
@@ -0,0 +1,17 @@
+use std::time::SystemTime;
+use log::{info, LevelFilter};
+use reqwest::header::ACCEPT;
+use sui_types::base_types::ObjectID;
+use sui_indexer::reader::CheckpointReader;
+use bcs::from_bytes;
+use sui_types::full_checkpoint_content::CheckpointData;
+
+#[tokio::test]
+async fn speed_test() {
+    let start = SystemTime::now();
+    env_logger::builder().filter_level(LevelFilter::Debug).init();
+    let client = reqwest::Client::new();
+    let result = client.get("http://localhost:6000/rest/checkpoints/27837398/full").header(ACCEPT, "application/bcs").send().await;
+    let r = from_bytes::<CheckpointData>(&result.unwrap().bytes().await.unwrap()).unwrap();
+    info!("{:?} {}", r.checkpoint_summary.epoch, start.elapsed().unwrap().as_millis());
+}
