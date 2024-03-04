6ca5bfe81863d9bc15d263b7de94449511f285ec
Norbaeocystin <baranrastislav@gmail.com>
2024-03-04 00:38:33 -0300

    fetch latest checkpoint

--- tests/rest_api_test.rs
+++ tests/rest_api_test.rs
@@ -12,8 +12,8 @@ async fn speed_test() {
     env_logger::builder().filter_level(LevelFilter::Debug).init();
     let client = reqwest::Client::new();
     // latest checkpoint
-    let result = client.get("http://localhost:6000/rest/checkpoints").header(ACCEPT, "application/bcs").send().await;
-    info!("{:?} {}", result, start.elapsed().unwrap().as_millis());
+    let result = client.get("http://localhost:6000/rest/checkpoints").send().await;
+    info!("{:?} {}", result.unwrap(), start.elapsed().unwrap().as_millis());
     let result = client.get("http://localhost:6000/rest/checkpoints/27837398/full").header(ACCEPT, "application/bcs").send().await;
     let r = from_bytes::<CheckpointData>(&result.unwrap().bytes().await.unwrap()).unwrap();
     info!("{:?} {}", r.checkpoint_summary.epoch, start.elapsed().unwrap().as_millis());
